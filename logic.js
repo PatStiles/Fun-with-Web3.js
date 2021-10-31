5ac5486601df25cdecd330516f1167678aee5341
Yuheng Chen <ccyyhh97@hotmail.com>
2021-10-31 12:14:08 -0500

    getbalance

--- logic.js
+++ logic.js
@@ -5,9 +5,7 @@ const url = "https://eth-mainnet.alchemyapi.io/v2/xSdpe0QgUO8vQG-IOvGL7Py5DkTOA3
 // Using web3js
 const web3 = new Web3(url);
 
-// Using ethers.js
-const provider =
-  new ethers.providers.JsonRpcProvider(url);
+web3.eth.getBalance("0xc6cf41f303968d1afb6ea1e5f0a95ce6f1dfbefd")
+.then(console.log);
 
 
-  
