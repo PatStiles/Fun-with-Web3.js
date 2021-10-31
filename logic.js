956d5609de119aad7b8ca8fecfa8b7ffb97c1c2d
Yuheng Chen <ccyyhh97@hotmail.com>
2021-10-31 12:10:43 -0500

    index.html

--- logic.js
+++ logic.js
@@ -1,3 +1,5 @@
+const Web3 = require('web3')
+
 const url = "https://eth-mainnet.alchemyapi.io/v2/xSdpe0QgUO8vQG-IOvGL7Py5DkTOA3d1";
 
 // Using web3js
@@ -6,3 +8,6 @@ const web3 = new Web3(url);
 // Using ethers.js
 const provider =
   new ethers.providers.JsonRpcProvider(url);
+
+
+  
