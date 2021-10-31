abb0572b791b7489d19d1eecf200545b24b75ac8
PatStiles <33334338+PatStiles@users.noreply.github.com>
2021-10-31 11:43:50 -0500

    Update logic.js
    added alchemy websocket configuration

---
+++ logic.js
@@ -0,0 +1,8 @@
+const url = "https://eth-mainnet.alchemyapi.io/v2/xSdpe0QgUO8vQG-IOvGL7Py5DkTOA3d1";
+
+// Using web3js
+const web3 = new Web3(url);
+
+// Using ethers.js
+const provider =
+  new ethers.providers.JsonRpcProvider(url);
